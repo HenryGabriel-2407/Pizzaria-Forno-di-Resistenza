4a84cab35bcf044343ca9b69e9162011910e3f49
Henry Gabriel <63942305+HenryGabriel-2407@users.noreply.github.com>
2026-06-14 21:00:23 -0300

    Ajustar em relação a sessão das requisições

--- pizzaria-system/pizzaria_system/database.py
+++ pizzaria-system/pizzaria_system/database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
+from sqlalchemy.orm import Session, sessionmaker
 from pizzaria_system.settings import Settings
 
 settings = Settings()
 engine = create_engine(settings.DATABASE_URL)
 
+# Cria uma fábrica de sessões, que é uma prática recomendada.
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_session():
-    yield Session(engine)
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
